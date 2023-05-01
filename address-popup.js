201163abb226496e28b14d47c2d6164d90dbb88e
Lucas Borges <lucaspdfborges@gmail.com>
2023-05-01 15:08:22 -0300

    fix conditional

--- address-popup.js
+++ address-popup.js
@@ -90,7 +90,7 @@ window.addEventListener('address', async function (e) {
     const address = getState('address');
     const addressInput = document.querySelector('#address-input');
     addressInput.value = address?.description || '';
-    addressOpenButton.innerHTML = address.description || 'Enter delivery address';
+    addressOpenButton.innerHTML = address?.description || 'Enter Delivery Address';
 
     const addressObj = await liquid.address({ placeId: address.placeId });
     const groupingIds = getState('grouping_ids');
